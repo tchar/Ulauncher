f1ca43b7edcf34259efe9d6380f46c778a5e1a9b
Aleksandr Gornostal <al.gornostal@gmail.com>
2019-04-28 18:14:58 +0300

    Issue #363: Unset GDK_BACKEND before launching apps

--- tests/api/shared/action/test_LaunchAppAction.py
+++ tests/api/shared/action/test_LaunchAppAction.py
@@ -14,11 +14,40 @@ class TestLaunchAppAction:
     def action(self, filename):
         return LaunchAppAction(filename)
 
+    @pytest.fixture(autouse=True)
+    def AppLaunchContext(self, mocker):
+        return mocker.patch('ulauncher.api.shared.action.LaunchAppAction.Gio.AppLaunchContext')
+
+    @pytest.fixture(autouse=True)
+    def read_desktop_file(self, mocker):
+        return mocker.patch('ulauncher.api.shared.action.LaunchAppAction.read_desktop_file')
+
+    @pytest.fixture(autouse=True)
+    def is_wayland(self, mocker):
+        return mocker.patch('ulauncher.api.shared.action.LaunchAppAction.is_wayland')
+
+    @pytest.fixture(autouse=True)
+    def is_wayland_compatibility_on(self, mocker):
+        return mocker.patch('ulauncher.api.shared.action.LaunchAppAction.is_wayland_compatibility_on')
+
+    @pytest.fixture(autouse=True)
+    def gdk_backend(self, mocker):
+        return mocker.patch('ulauncher.api.shared.action.LaunchAppAction.gdk_backend')
+
     def test_keep_app_open(self, action):
         assert not action.keep_app_open()
 
-    def test_run(self, action, mocker, filename):
-        read_desktop_file = mocker.patch('ulauncher.api.shared.action.LaunchAppAction.read_desktop_file')
+    def test_run(self, action, filename, read_desktop_file, AppLaunchContext):
         action.run()
         read_desktop_file.assert_called_with(filename)
-        read_desktop_file.return_value.launch.assert_called_with()
+        read_desktop_file.return_value.launch.assert_called_with(None, AppLaunchContext.return_value)
+
+    def test_run__is_wayland__unset_env_var(self, action, AppLaunchContext, is_wayland, gdk_backend,
+                                            is_wayland_compatibility_on):
+        is_wayland.return_value = True
+        gdk_backend.return_value.lower.return_value = 'x11'
+        is_wayland_compatibility_on.return_value = False
+
+        action.run()
+
+        AppLaunchContext.return_value.unsetenv.assert_called_with('GDK_BACKEND')
--- ulauncher/api/shared/action/LaunchAppAction.py
+++ ulauncher/api/shared/action/LaunchAppAction.py
@@ -1,9 +1,10 @@
 import logging
+from gi.repository import Gio
 
 from ulauncher.util.desktop.reader import read_desktop_file
 from ulauncher.util.string import force_unicode
+from ulauncher.config import is_wayland, is_wayland_compatibility_on, gdk_backend
 from .BaseAction import BaseAction
-
 logger = logging.getLogger(__name__)
 
 
@@ -23,4 +24,10 @@ class LaunchAppAction(BaseAction):
     def run(self):
         app = read_desktop_file(self.filename)
         logger.info('Run application %s (%s)' % (force_unicode(app.get_name()), self.filename))
-        app.launch()
+        context = Gio.AppLaunchContext()
+
+        # Unset GDK_BACKEND if we forced a user to set it
+        if is_wayland() and gdk_backend().lower() == 'x11' and not is_wayland_compatibility_on():
+            context.unsetenv('GDK_BACKEND')
+
+        app.launch(None, context)
