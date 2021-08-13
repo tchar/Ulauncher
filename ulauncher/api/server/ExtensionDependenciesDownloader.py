3de250c63a7c477521f58d6e58e0eb4d0ac3306e
tchar <tilemachos.charalampous@gmail.com>
2021-08-13 19:33:40 +0300

    Update ExtensionDependenciesDownloader to use venv module

--- ulauncher/api/server/ExtensionDependenciesDownloader.py
+++ ulauncher/api/server/ExtensionDependenciesDownloader.py
@@ -1,98 +1,88 @@
-import sys
+from typing import Callable, BinaryIO, Union
+from types import SimpleNamespace
 import os
-from typing import Optional
-import logging
 from shutil import rmtree
-from subprocess import Popen, PIPE
+import sys
+import venv
+import logging
+from subprocess import Popen, PIPE, CalledProcessError
+from ulauncher.utils.decorator.run_async import run_async
 
 logger = logging.getLogger(__name__)
 
 
-class ExtensionDependenciesDownloader:
+class ExtensionDependenciesDownloader(venv.EnvBuilder):
     def __init__(self, ext_path: str) -> None:
-        self.ext_path = ext_path
-        self.venv_path: Optional[str] = None
-
-    @property
-    def executable(self):
-        """Path of the python executable"""
-        if self.venv_path is None:
-            return sys.executable
-        return os.path.join(self.venv_path, 'bin', 'python')
+        super().__init__(
+            system_site_packages=True,
+            clear=False,
+            upgrade=False,
+            symlinks=True,
+            with_pip=True,
+        )
+        self.executable = sys.executable
+        self.ext_path = os.path.abspath(ext_path)
 
     @property
     def requirements_file(self) -> str:
-        """Path of requirements file"""
         return os.path.join(self.ext_path, 'requirements.txt')
 
-    def needs_venv(self) -> bool:
-        """Returns True if requirements file is found, False otherwise"""
-        requirements_file = self.requirements_file
-        if not os.path.isfile(requirements_file):
-            return False
+    @run_async
+    def reader(self, stream: BinaryIO, log_cb: Callable[[str], None]) -> None:
+        """
+        Read lines from a subprocess' output stream and log it
+        """
+        while True:
+            s = stream.readline()
+            if not s:
+                break
+            log_cb(s.decode('utf-8').rstrip('\n'))
+        stream.close()
 
-        logger.debug('Requirements file found: %s', requirements_file)
-        return True
+    def post_setup(self, context: SimpleNamespace) -> None:
+        """
+        Installs requirements
+        """
+        args = [context.env_exe, '-m', 'pip', 'install', '-r', self.requirements_file]
+        proc = Popen(args, stdout=PIPE, stderr=PIPE)
+        t1 = run_async(self.reader)(proc.stdout, logger.info)
+        t2 = run_async(self.reader)(proc.stderr, logger.warning)
+        proc.wait()
+        t1.join()
+        t2.join()
+        if proc.returncode:
+            raise CalledProcessError(proc.returncode, args)
 
-    def create_venv(self) -> Optional[str]:
-        """Creates venv and returns the venv path if venv is created, None otherwise"""
-        if self.venv_path is not None:
-            return self.venv_path
-
-        venv_name = os.path.basename(self.ext_path) + '_venv'
-        venv_path = os.path.join(self.ext_path, venv_name)
-        if os.path.exists(venv_path):
-            logger.warning('Venv path already exists: %s', venv_path)
-            return None
-
-        args = [sys.executable, '-m', 'venv', '--system-site-packages', venv_path]
+    def create(self, env_dir: Union[str, bytes, os.PathLike]) -> None:
+        """
+        Creates the venv
+        """
         try:
-            proc = Popen(args, stdout=PIPE, stderr=PIPE)
-        except FileNotFoundError as e:
-            logger.exception(e)
-            return None
+            context = self.ensure_directories(env_dir)
+            # See issue 24875 from venv. We need system_site_packages to be False
+            # until after pip is installed.
+            self.system_site_packages = False
+            self.create_configuration(context)
+            self.setup_python(context)
+            self._setup_pip(context)
+            self.setup_scripts(context)
+            self.post_setup(context)
+            self.system_site_packages = True
+            self.create_configuration(context)
+            self.executable = context.env_exe
+        # This catch is also used for self._setup_pip() in case ensurepip is missing
+        except CalledProcessError as e:
+            if os.path.isdir(env_dir):
+                rmtree(env_dir)
+            logger.error(e)
 
-        out, err = proc.communicate()
-        if out:
-            logger.debug(out.decode('utf-8'))
-        if err:
-            logger.warning(err.decode('utf-8'))
-
-        if proc.returncode != 0:
-            logger.debug('%s returned %d', ' '.join(args), proc.returncode)
-            return None
-        logger.debug('Created venv %s', venv_path)
-        self.venv_path = venv_path
-        return venv_path
-
-    def remove_venv(self) -> None:
-        """Removes the venv created"""
-        if self.venv_path is None:
+    def download(self) -> None:
+        """
+        Downloads dependencies if required. After download is called and if any dependencies
+        were installed, self.executable will point to the python executable inside the venv.
+        """
+        ext_basename = os.path.basename(self.ext_path)
+        env_dir = os.path.join(self.ext_path, ext_basename + '_venv')
+        if os.path.exists(env_dir) or not os.path.exists(self.requirements_file):
             return
-        rmtree(self.venv_path)
-        self.venv_path = None
-
-    def download(self) -> bool:
-        """Downloads dependencies if needed and returns True if downloaded, False otherwise"""
-        if not self.needs_venv() or not self.create_venv():
-            return False
-
-        args = [self.executable, '-m', 'pip', 'install', '-r', self.requirements_file]
-        try:
-            proc = Popen(args, stdout=PIPE, stderr=PIPE)
-        except FileNotFoundError as e:
-            logger.exception(e)
-            self.remove_venv()
-            return False
-
-        out, err = proc.communicate()
-        if out:
-            logger.debug(out.decode('utf-8'))
-        if err:
-            logger.warning(err.decode('utf-8'))
-
-        if proc.returncode != 0:
-            self.remove_venv()
-            return False
-
-        return True
+        self.create(env_dir)
